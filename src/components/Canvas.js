92445c63a6f73d2c21deff2cd41c6ff76537ac7d
JuanLP <juan.luis.palacios.p@gmail.com>
2022-11-02 18:57:59 -0500

    Add basic Canvas logic

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,10 +1,31 @@
+import { createContext, useEffect, useState } from 'react';
 import '../css/Canvas.css';
 
-function Canvas() {
-  return (
-    <div className="Canvas">
+const UserContext = createContext();
 
-    </div>
+function Canvas(props) {
+    const [[canvasElement,context],setContext] = useState([null,null]);
+    const {width, height, brush, children} = props
+    useEffect(()=>{
+        if(canvasElement == null){
+            let canvas = document.createElement('canvas');
+            setContext([canvas, canvas.getContext("2d")]);
+        }
+    }, [])
+    useEffect(()=>{
+        canvasElement.width = width
+    }, [width])
+    useEffect(()=>{
+        canvasElement.height = height
+    }, [height])
+    useEffect(()=>{
+
+    }, [brush])
+  return (
+    <UserContext.Provider value={{context, brush}}>
+      {canvasElement}
+      {children}
+    </UserContext.Provider>
   );
 }
 

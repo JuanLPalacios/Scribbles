426f9cf572a61c28fd4ac0e253f8c31f9bf88557
JuanLP <juan.luis.palacios.p@gmail.com>
2022-11-03 23:56:02 -0500

    Add basic layer logic

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -1,11 +1,34 @@
+import { useContext, useEffect, useState } from 'react';
 import '../css/Layer.css';
 
-function Layer() {
-  return (
-    <div className="Layer">
-
-    </div>
-  );
+function Layer(props) {
+  const [[canvasElement,layerContext],setContext] = useState([null,null]);
+  const {x, y, width, height} = props
+  const {brush} = useContext()
+  useEffect(()=>{
+      if(canvasElement == null){
+          let canvas = document.createElement('canvas');
+          setContext([canvas, canvas.getContext("2d")]);
+      }
+  }, [])
+  useEffect(()=>{
+    canvasElement.left = x
+}, [x])
+useEffect(()=>{
+    canvasElement.top = y
+}, [y])
+useEffect(()=>{
+  canvasElement.width = width
+}, [width])
+useEffect(()=>{
+  canvasElement.height = height
+}, [height])
+useEffect(()=>{
+  brush.setContext(layerContext)
+  }, [brush])
+return (
+    {canvasElement}
+);
 }
 
 export default Layer;

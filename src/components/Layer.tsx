1d991fc23bbacab45a23ce74b4eae4a6f9ff39d9
JuanLP <juan.luis.palacios.p@gmail.com>
2022-11-16 23:36:18 -0500

    Refactor layer to work with Drawable

--- src/components/Layer.tsx
+++ src/components/Layer.tsx
@@ -4,9 +4,22 @@ import {
 import { Rect } from '../types/Rect';
 import '../css/Layer.css';
 import React from 'react';
+import { Drawable, DrawableState } from './Drawable';
+import { buffer } from 'stream/consumers';
 
-const Layer = forwardRef<{canvas:HTMLCanvasElement|null, buffer:HTMLCanvasElement|null},{rect:Rect}>((props, ref) => {
-  const layerRef = useRef<{canvas:HTMLCanvasElement|null, buffer:HTMLCanvasElement|null} | null>({canvas:null, buffer:null});
+export interface LayerState {
+  key: number;
+  rect: Rect;
+  name: string;
+  canvas:DrawableState
+  buffers: DrawableState[];
+  thumbnail: DrawableState;
+  selected:boolean
+}
+
+
+const Layer = forwardRef<LayerState,LayerState>((props, ref) => {
+  const layerRef = useRef<LayerState>(props);
   const [
     x, y, width, height,
    ] = props.rect;
@@ -15,10 +28,46 @@ const Layer = forwardRef<{canvas:HTMLCanvasElement|null, buffer:HTMLCanvasElemen
     if(ref !== null)
       (ref as any)(layer);
   }, []);
+
+  function resizeThumbnail() {
+    if (!props.thumbnail) return;
+    if (!props.canvas) return;
+    const width = props.thumbnail.rect[2];
+    const height = props.thumbnail.rect[2] * (props.canvas.rect[3] / props.canvas.rect[2]);
+    throw 'rezise hook not implemented';
+  }
+  
+  function renderThumbnail() {
+    if (!props.thumbnail.canvas) return;
+    if (!props.canvas.canvas) return;
+    props.thumbnail.ctx?.drawImage(props.canvas.canvas, 0, 0, props.thumbnail.canvas.width, props.thumbnail.canvas.height);
+  }
+
+  const setCanvas = (canvas:DrawableState | undefined) => {
+    if (!canvas) return;
+    if(typeof ref === 'function')
+      ref({...props, canvas});
+    resizeThumbnail();
+  }
+
+  const setBuffer = (buffer:DrawableState | undefined) => {
+    if (!buffer) return;
+    const buffers = [buffer];
+    throw 'setBuffers hook not implemented';
+  }
+
+  const setThumbnail = (thubnail:DrawableState | undefined) => {
+    if (!thubnail) return;
+    if (props.thumbnail === thubnail) return;
+    props.thumbnail = thubnail;
+    resizeThumbnail();
+    renderThumbnail();
+  }
+
   return (
     <div style={{ left: `${x}px`, top: `${y}px` }}>
-      <canvas ref={(canvas) => {if(layerRef.current)layerRef.current.canvas = canvas}} {...{ width, height }} />
-      <canvas ref={(buffer) => {if(layerRef.current)layerRef.current.buffer = buffer}} {...{ width, height }} />
+      <Drawable rect={props.rect} ref={setCanvas} />
+      <Drawable rect={props.rect} ref={setBuffer} />
     </div>
   );
 });

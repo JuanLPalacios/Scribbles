939494cbf178edca65b02077cc876d5e7e0c541e
JuanLP <juan.luis.palacios.p@gmail.com>
2022-11-26 19:58:24 -0500

    Add rotation handlers

--- src/tools/Transform.ts
+++ src/tools/Transform.ts
@@ -1,15 +1,28 @@
 import Brush from '../abstracts/Brush';
 import Tool from '../abstracts/Tool';
 import { LayerState } from '../types/LayerState';
+import { Point } from '../types/Point';
   
 export const transform = new (class Transform extends Tool {
+    center: Point = [0,0];
+    rotation = 0;
+
+
+
     mouseDown(brush:Brush, {nativeEvent}:React.MouseEvent, layer:LayerState, color:string, width:number) {
         nativeEvent.preventDefault();
     }
 
     mouseMove(brush:Brush, e:React.MouseEvent, layer:LayerState, color:string, width:number) {
         const {nativeEvent} = e;
+        const {currentTarget} = <{currentTarget:HTMLElement}><unknown>e;
         nativeEvent.preventDefault();
+        
+        console.log(
+            'currentTarget',
+            currentTarget.offsetLeft,
+            currentTarget.offsetTop
+        );
         console.log(
             'page',
             e.pageX,
@@ -41,6 +54,8 @@ export const transform = new (class Transform extends Tool {
             nativeEvent.offsetY
         );
     }
+
+
     
     mouseUp(brush:Brush, {nativeEvent}:React.MouseEvent, layer:LayerState, color:string, width:number) {
         nativeEvent.preventDefault();
@@ -53,5 +68,46 @@ export const transform = new (class Transform extends Tool {
             nativeEvent.offsetX,
             nativeEvent.offsetY
         );
+    }
+
+    startTranslation(e:React.MouseEvent){
+        throw new Error('Method not implemented.');
+    }
+
+    translate(e:React.MouseEvent){
+        throw new Error('Method not implemented.');
+    }
+
+    startSkewering(e:React.MouseEvent){
+        throw new Error('Method not implemented.');
+    }
+
+    skew(e:React.MouseEvent){
+        throw new Error('Method not implemented.');
+    }
+
+    startScaling(e:React.MouseEvent){
+        throw new Error('Method not implemented.');
+    }
+
+    scale(e:React.MouseEvent){
+        throw new Error('Method not implemented.');
+    }
+
+    startRotation(e:React.MouseEvent, layer:LayerState){
+        const {currentTarget} = <{currentTarget:HTMLElement}><unknown>e;
+        const arrowRects = currentTarget.getBoundingClientRect();
+        this.center = [arrowRects.left + arrowRects.width / 2, arrowRects.top + arrowRects.height / 2];
+        layer.buffer.ctx?.rotate();;
+    }
+
+    rotate(e:React.MouseEvent, layer:LayerState){
+        const angle = Math.atan2(e.clientY - this.center[1], e.clientX - this.center[0]) + Math.PI / 2;
+        layer.buffer.ctx?.rotate(angle * 180 / Math.PI);
+        this.render(layer);
+    }
+
+    render(layer: LayerState) {
+        throw new Error('Method not implemented.');
     }
 })();

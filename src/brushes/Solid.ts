641d25db73704d99ee8db0b6b3b09736da9a90f3
JuanLP <juan.luis.palacios.p@gmail.com>
2022-12-14 21:45:05 -0500

    Add solid brush

---
+++ src/brushes/Solid.ts
@@ -0,0 +1,29 @@
+import Brush from '../abstracts/Brush';
+import { DrawableState } from '../types/DrawableState';
+import { Point } from '../types/Point';
+
+export default class Solid extends Brush {
+    startStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
+        const { ctx } = drawable;
+        if (!ctx) return;
+        ctx.globalCompositeOperation = 'destination-over';
+        ctx.globalAlpha = alpha;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.beginPath();
+        ctx.moveTo(...point);
+    }
+
+    drawStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
+        const { ctx, canvas } = drawable;
+        if (!ctx) return;
+        ctx.lineTo(...point);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.stroke();
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    endStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {}
+}

ab73f49b1548840a27fb74bf78653d6ce1309b73
JuanLP <juan.luis.palacios.p@gmail.com>
2024-05-06 15:24:52 -0500

    Add support for .abr format header and brush structure for vers 1,2,6.1,6.2

---
+++ src/brushes/AbrBrush.ts
@@ -0,0 +1,176 @@
+import { SequentialDataView } from '../lib/SequentialDataView';
+import { AbrHeader } from './AbrInfo';
+
+export async function loadAbrBrushes(file: File) {
+    const buffer:ArrayBuffer = await (new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.onload = function(event) {
+            if(event.target&&event.target.result&&(typeof event.target.result != 'string')) resolve(event.target.result);
+            else reject();
+        };
+        fileReader.readAsArrayBuffer(file);
+    }));
+    return loadAbrFromArrayBuffer(buffer, file.name);
+}
+
+export async function loadAbrFromArrayBuffer(buffer: ArrayBuffer, filename:string) {
+    const abrSdv = new SequentialDataView(buffer);
+
+    try {
+        const abrHeader:AbrHeader = abrReadContent(abrSdv);
+        if (!abrSupportedContent(abrHeader)) {
+            throw new Error(`ERROR: unable to decode abr format version ${abrHeader.version}(subver ${abrHeader.subversion})`);
+        }
+
+        if (abrHeader.count == 0) {
+            throw new Error(`ERROR: no brushes found in ${filename}`);
+            return false;
+        }
+
+        const imageId = 123456;
+
+        for (let i = 0; i < abrHeader.count; i++) {
+            const layerId = await abrBrushLoad(abrSdv, abrHeader, filename, imageId, i + 1);
+            if (layerId == -1) {
+                console.warn(`Warning: problem loading brush #${i} in ${filename}`);
+            }
+        }
+
+        return true;
+    } catch (error) {
+        throw new Error(`Error: cannot parse ABR file: ${filename}`, { cause: error });
+    }
+}
+
+export const abrReadContent=(abrSdv:SequentialDataView):AbrHeader=>{
+    const abrHeader:AbrHeader = { count: 0, subversion: 0, version: 0 };
+    abrHeader.version = abrSdv.getUint16();
+    abrHeader.subversion = 0;
+    abrHeader.count = 0;
+
+    switch (abrHeader.version) {
+    case 1:
+    case 2:
+        // ver:1-2
+        abrHeader.count = abrSdv.getUint16();
+        break;
+    case 6:
+        // ver:6
+        abrHeader.subversion = abrSdv.getUint16();
+        abrHeader.count = findSampleCountV6(abrSdv, abrHeader);
+        break;
+    default:
+        // unknown versions
+        break;
+    }
+
+    // next bytes in abr are samples data
+    return abrHeader;
+};
+
+function findSampleCountV6(abrSdv: SequentialDataView, abrHeader: AbrHeader): number {
+    let brushSize:number;
+    let brushEnd:number;
+
+    if (!abrSupportedContent(abrHeader))
+        return 0;
+
+    const origin = abrSdv.getPos();
+    try {
+        abrReach8BimSection(abrSdv, 'samp');
+    } catch (error) {
+        abrSdv.setPos(origin);
+        return 0;
+    }
+
+    // long
+    const sampleSectionSize = abrSdv.getUint32();
+    const sampleSectionEnd = sampleSectionSize + abrSdv.getPos();
+
+    if(sampleSectionEnd < 0 || sampleSectionEnd > abrSdv.size())
+        return 0;
+
+    const dataStart = abrSdv.getPos();
+    let samples = 0;
+    while ((!abrSdv.atEnd()) && (abrSdv.getPos() < sampleSectionEnd)) {
+        // read long
+        brushSize = abrSdv.getUint32();
+        brushEnd = brushSize;
+        // complement to 4
+        while (brushEnd % 4 != 0) brushEnd++;
+
+        const newPos = abrSdv.getPos() + brushEnd;
+        if(newPos > 0 && newPos < abrSdv.size()) {
+            abrSdv.setPos(newPos);
+        }
+        else
+            return 0;
+
+        samples++;
+    }
+
+    // set StreamDataViewer to samples data
+    abrSdv.setPos(dataStart);
+
+    return samples;
+}
+
+export function abrSupportedContent(abrHeader: AbrHeader): boolean {
+    switch (abrHeader.version) {
+    case 1:
+    case 2:
+        return true;
+    case 6:
+        if (abrHeader.subversion == 1 || abrHeader.subversion == 2)
+            return true;
+        break;
+    }
+    return false;
+}
+export function abrReach8BimSection(abrSdv: SequentialDataView, name: string):void {
+    let sectionSize = 0;
+    // find 8BIMname section
+    while (!abrSdv.atEnd()) {
+        const tag: number[] = [];
+        const tagname: number[] = [];
+        let r;
+        r = abrSdv.readRawData(tag, 4);
+        if (r != 4) {
+            throw new Error('Error: Cannot read 8BIM tag ');
+        }
+
+        if (charCodeComparison(tag, '8BIM', 4)) {
+            throw new Error('Error: Start tag not 8BIM but ' + String.fromCharCode(...tag) + ' at position ' + abrSdv.getPos());
+        }
+
+        r = abrSdv.readRawData(tagname, 4);
+
+        if (r != 4) {
+            throw new Error('Error: Cannot read 8BIM tag name');
+        }
+
+        const s1: string = String.fromCharCode(...tagname);
+
+        if (s1 == name) {
+            return;
+        }
+
+        // long
+        sectionSize = abrSdv.getUint32();
+        abrSdv.setPos(abrSdv.getPos() + sectionSize);
+    }
+    return;
+}
+
+export function charCodeComparison(buffer: number[], str: string, num: number): boolean {
+    if ((buffer.length < num) || (str.length < num)) return true;
+    for (let i = 0; i < num; i++) {
+        if (str.charCodeAt(i) !== buffer[i]) return true;
+    }
+    return false;
+}
+
+function abrBrushLoad(abrSdv: SequentialDataView, abrHeader: AbrHeader, name: string, imageId: number, arg4: number):number {
+    throw new Error('Function not implemented.');
+}
+

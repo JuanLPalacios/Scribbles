d38701f197c3036db8c532b16613bbe7890c30c3
JuanLP <juan.luis.palacios.p@gmail.com>
2024-05-19 23:55:02 -0500

    Add selectedBrush controls

--- src/components/menu/ImportBrush.tsx
+++ src/components/menu/ImportBrush.tsx
@@ -11,6 +11,15 @@ import { createDrawable } from '../../generators/createDrawable';
 import Brush from '../../abstracts/Brush';
 import { Drawable } from '../Drawable';
 import { uid } from '../../lib/uid';
+import { abrBrushes } from 'abr-js';
+
+type ScribbleBrush = {
+    scribbleBrushType: 1
+}
+type AbrBrushWrapper = {
+    scribbleBrushType: 2
+} & AbrBrush;
+type Brush = ScribbleBrush | AbrBrushWrapper
 
 let previews:{previews:DrawableState[], selectedPreview:DrawableState}|undefined;
 let lastBrushes: Brush[];
@@ -22,11 +31,21 @@ export const ImportBrush = () => {
         brushes
     } = options;
     const [id] = useState(uid());
+    const [currentBrush, setBrush] = useState<Brush>({
+        scribbleBrushType: 2,
+        brushType: 1,
+        angle: 0,
+        diameter: 1,
+        hardness: 1,
+        name: '',
+        roundness: 1,
+        spacing: 2
+    });
     const [state2, onChange] = useState({ selectedBrush: 0 });
     const { selectedBrush } = state2;
     const [editor, editorDispatch] = useContext(EditorContext);
-    const [state, setState] = useState({ isOpen: false, name: '', width: 600, height: 600, isValid: false, errors: { name: new Array<string>(), width: new Array<string>(), height: new Array<string>() } });
-    const { isOpen, name, width, height, isValid, errors } = state;
+    const [state, setState] = useState({ isOpen: false, isValid: false, errors: { name: new Array<string>(), width: new Array<string>(), height: new Array<string>() } });
+    const { isOpen, isValid, errors } = state;
     const update = useCallback((e:React.ChangeEvent<HTMLInputElement>) => {
         let value;
         switch (e.target.type){
@@ -37,14 +56,13 @@ export const ImportBrush = () => {
             value = e.target.value;
             break;
         }
-        setState({ ...state, [e.target?.name]: value });
+        setBrush({ ...currentBrush, [e.target?.name]: value });
     }, [state]);
     const close = useCallback(() => {
         setState({ ...state, isOpen: false });
     }, [state]);
     const openModal = useCallback(() => {
-        const { width, height } = editor.drawing || { width: 600, height: 600 };
-        setState({ ...state, isOpen: true, width, height });
+        setState({ ...state, isOpen: true });
     }, [editor, state]);
     const newfile = useCallback(() => {
         editorDispatch({
@@ -68,15 +86,15 @@ export const ImportBrush = () => {
             }
         });
         close();
-    }, [close, height, name, editorDispatch, width]);
+    }, [close, editorDispatch]);
     useEffect(() => {
         const errors = { name: new Array<string>(), width: new Array<string>(), height: new Array<string>() };
-        if(name.length<1)
-            errors.name.push('Must have at least 1 character');
-        if(name.match(/[.,#%&{}\\<>*?/$!'":@+`|=]/gi))
-            errors.name.push('Shuld not contain forbidden characters');
+        if(currentBrush.scribbleBrushType==2)
+            if(currentBrush.brushType==1)
+                if(currentBrush.name.match(/[.,#%&{}\\<>*?/$!'":@+`|=]/gi))
+                    errors.name.push('Shuld not contain forbidden characters');
         setState({ ...state, errors, isValid: Object.values(errors).reduce((total, value)=> total + value.length, 0) === 0 });
-    }, [height, name, width]);
+    }, [currentBrush]);
     useEffect(()=>{
         if(lastBrushes !== brushes){
             lastBrushes = brushes;
@@ -104,21 +122,24 @@ export const ImportBrush = () => {
                             {previews?.previews.map(({ canvas }, i) => <li key={id+'-'+i}><Drawable canvas={canvas} className={i==selectedBrush ? 'selected' : ''} onMouseDown={()=>onChange({ ...state2, selectedBrush: i })} /></li>)}
                         </ul>
                     </div>
-                    <div style={{ width: '7rem' }}>
-                        <label>
-                            Name
-                            <input type="text" name='name' autoComplete="off" value={name} onChange={update} />
-                        </label>
-                        <label>
-                            Width
-                            <input type="number" min={1} name='width' value={width} onChange={update} />
-                        </label>
-                        <label>
-                            Height
-                            <input type="number" min={1} name='height' value={height} onChange={update} />
-                        </label>
+                    {currentBrush.scribbleBrushType==2 &&
+                        currentBrush.brushType==1 &&
+                        <div style={{ width: '7rem' }}>
+                            <label>
+                                Name
+                                <input type="text" name='name' autoComplete="off" value={currentBrush.name} onChange={update} />
+                            </label>
+                            <label>
+                                Diameter
+                                <input type="number" min={1} name='diameter' value={currentBrush.diameter} onChange={update} />
+                            </label>
+                            <label>
+                                Spacing
+                                <input type="number" min={0} name='spacing' value={currentBrush.spacing} onChange={update} />
+                            </label>
 
-                    </div>
+                        </div>
+                    }
                 </div>
                 <div className='actions'>
                     <button onClick={newfile} disabled={!isValid}>create</button>

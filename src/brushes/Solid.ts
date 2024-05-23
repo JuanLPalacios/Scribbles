ddc5d9b6d1e9f528aff069b4ffd767f14afe035b
JuanLP <juan.luis.palacios.p@gmail.com>
2024-05-22 23:52:25 -0500

    Add SolidBrush support for irregular brush shape props

--- src/brushes/Solid.ts
+++ src/brushes/Solid.ts
@@ -5,10 +5,22 @@ import { Point } from '../types/Point';
 type SerializedSolidBrush ={
     scribbleBrushType: 2,
     name:string
+    angle: number,
+    diameter: number,
+    hardness: number,
+    roundness: number,
+    spacing: number
 }
 
 export default class Solid extends Brush {
+    lastPoint:Point = [0, 0]
+    segments:[Point, Point][] = []
     name = 'Solid';
+    angle = 1;
+    diameter = 1;
+    hardness = 1;
+    roundness = 1;
+    spacing = 1;
     startStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
         const { ctx } = drawable;
         if (!ctx) return;
@@ -18,16 +30,19 @@ export default class Solid extends Brush {
         ctx.lineJoin = 'round';
         ctx.strokeStyle = color;
         ctx.lineWidth = width;
+        this.lastPoint = point;
         ctx.beginPath();
         ctx.moveTo(...point);
     }
 
     drawStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
-        const { ctx, canvas } = drawable;
+        const { ctx } = drawable;
         if (!ctx) return;
+        ctx.beginPath();
+        ctx.moveTo(...this.lastPoint);
         ctx.lineTo(...point);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.stroke();
+        //ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 
     endStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
@@ -36,12 +51,24 @@ export default class Solid extends Brush {
     }
 
     toObj(): SerializedSolidBrush {
-        const { name } = this;
-        return { scribbleBrushType: 2, name };
+        const { name, angle, diameter, hardness, roundness, spacing } = this;
+        return { scribbleBrushType: 2,
+            name,
+            angle,
+            diameter,
+            hardness,
+            roundness,
+            spacing
+        };
     }
 
-    loadObj({ name }:SerializedSolidBrush) {
+    loadObj({ name, angle, diameter, hardness, roundness, spacing }:SerializedSolidBrush) {
         this.name = name;
+        this.angle = angle;
+        this.diameter = diameter;
+        this.hardness = hardness;
+        this.roundness = roundness;
+        this.spacing = spacing;
     }
 
     static formObj(data:SerializedSolidBrush):Solid {

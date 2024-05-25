71bb2274033be8e533ff405796be3ff260e2f8fc
JuanLP <juan.luis.palacios.p@gmail.com>
2024-05-25 08:41:55 -0500

    Add buffer to solid brush to fix alpha segmented rendering

--- src/brushes/Solid.ts
+++ src/brushes/Solid.ts
@@ -1,4 +1,5 @@
 import Brush from '../abstracts/Brush';
+import { createDrawable } from '../generators/createDrawable';
 import { DrawableState } from '../types/DrawableState';
 import { Point } from '../types/Point';
 
@@ -20,36 +21,51 @@ export default class Solid extends Brush {
     strokeAngle = [0, -1];
     diameter = 1;
     hardness = 1;
-    roundness = .5;
+    roundness = 1;
     spacing = 1;
+    buffer:DrawableState = createDrawable({ size: [1, 1] });
     startStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
-        const { ctx } = drawable;
+        const { ctx, canvas } = drawable;
+        const { ctx: bufferCtx, canvas: buffer } = this.buffer;
+        buffer.width = canvas.width;
+        buffer.height = canvas.height;
+        if (!ctx || !bufferCtx) return;
+        bufferCtx.lineCap = 'round';
+        bufferCtx.lineJoin = 'round';
+        bufferCtx.strokeStyle = color;
+        bufferCtx.lineWidth = width;
         if (!ctx) return;
         ctx.globalCompositeOperation = 'source-over';
+        bufferCtx.globalCompositeOperation = 'source-over';
         ctx.globalAlpha = alpha;
-        ctx.lineCap = 'round';
-        ctx.lineJoin = 'round';
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width;
         this.lastPoint = point;
+        bufferCtx.beginPath();
+        bufferCtx.moveTo(...point);
+        bufferCtx.lineTo(...point);
+        // FIXME: draw tip shape to create the illusion of the more complex brush
+        bufferCtx.stroke();
+        ctx.drawImage(buffer, 0, 0);
     }
 
     drawStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
-        const { ctx } = drawable;
+        const { ctx, canvas } = drawable;
+        const { ctx: bufferCtx, canvas: buffer } = this.buffer;
         const v2 = [point[0]-this.lastPoint[0],  point[1]-this.lastPoint[1]];
         const v1 = this.strokeAngle;
-        if (!ctx) return;
-        ctx.beginPath();
-        ctx.moveTo(...this.lastPoint);
-        ctx.lineWidth = width*(1-(1-this.roundness)*(v1[0]*v2[1] - v1[1]*v2[0])*(Math.sqrt((v2[0]**2)+(v2[1]**2))));
-        ctx.lineTo(...point);
-        ctx.stroke();
+        if (!ctx || !bufferCtx) return;
+        bufferCtx.beginPath();
+        bufferCtx.moveTo(...this.lastPoint);
+        bufferCtx.lineWidth = width*(1-(1-this.roundness)*(Math.abs(v1[0]*v2[1] - v1[1]*v2[0]))/(Math.sqrt((v2[0]**2)+(v2[1]**2))));
+        bufferCtx.lineTo(...point);
+        bufferCtx.stroke();
         this.lastPoint = point;
-        //ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(buffer, 0, 0);
     }
 
     endStroke(drawable:DrawableState, point:Point, color:string, alpha:number, width:number) {
         const { ctx, } = drawable;
+        // FIXME: draw tip shape to create the illusion of the more complex brush
         ctx?.restore();
     }
 

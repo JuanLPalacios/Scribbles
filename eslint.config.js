1cdbd5f3193cb0c7a89f118580781c89bfc2b5c8
JuanLP <juan.luis.palacios.p@gmail.com>
2024-08-28 17:30:27 -0500

    Update dependencies and eslint config

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { "allowConstantExport": true }
+      ],
+      "react/jsx-filename-extension": ["warn", { "extensions": [".js", ".jsx", ".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "import/no": "off",
+      "import/no-unresolved": "off",
+      "import/extensions": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "args": "none" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "array-bracket-spacing": "error",
+      "block-spacing": "error",
+      "comma-spacing": "error",
+      "computed-property-spacing": "error",
+      "func-call-spacing": "error",
+      "key-spacing": "error",
+      "newline-per-chained-call": "error",
+      "no-multiple-empty-lines": ["error", { "max": 1}],
+      "no-shadow": "off",
+      "no-trailing-spaces": "error",
+      "no-underscore-dangle": "off",
+      "no-whitespace-before-property": "error",
+      "object-curly-spacing": ["error", "always"],
+      "padded-blocks": ["error", "never"],
+      "space-in-parens": "error",
+      "indent": [
+        "error",
+        4
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ]
+    },
+  },
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,10 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            '~': path.resolve(__dirname, './src'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@components': path.resolve(__dirname, './src/components'),
-            '@ui': path.resolve(__dirname, './src/components/ui'),
-            '@api': path.resolve(__dirname, './src/api'),
-            '@hooks': path.resolve(__dirname, './src/hooks'),
-            '@scripts': path.resolve(__dirname, './src/scripts'),
-            '@store': path.resolve(__dirname, './src/store'),
-            '@pages': path.resolve(__dirname, './src/pages'),
-            '@styles': path.resolve(__dirname, './src/styles'),
-        },
-    },
     plugins: [react(), svgr({}), VitePWA({
         manifest: {
             screenshots: [
@@ -50,10 +36,5 @@ export default defineConfig({
     server: {
         host: '127.0.0.1',
         port: 3000,
-    },
-    build: {
-        rollupOptions: {
-            external: [/^node:\w+/],
-        },
     },
 });

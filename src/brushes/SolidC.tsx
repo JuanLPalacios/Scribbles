95468907cea233bb085686335ff24f9b3099eb1f
JuanLP <juan.luis.palacios.p@gmail.com>
2024-09-11 22:52:47 -0500

    Add functional component MarkerBrush MarkerC

--- src/brushes/SolidC.tsx
+++ src/brushes/SolidC.tsx
@@ -6,8 +6,8 @@ import { Point } from '../types/Point';
 import { Serialized } from '../lib/Serialization';
 import { SerializedSolidBrush } from './Solid';
 import { SerializedTextureBrush } from './TextureBrush';
-import { SerializedImage } from './SerializedOject';
 import { BrushList } from '../lib/BrushList';
+import { SerializedMarkerBrush } from './Marker';
 
 type IsSerialized<S extends Serialized> = S;
 
@@ -322,3 +322,79 @@ export const  TextureC = BrushHOC(({ that, hooks: { setDrawStroke, setEndStroke,
     return <></>;
 });
 
+export const  MarkerC = BrushHOC(({ that, hooks: { setDrawStroke, setEndStroke, setRenderPreview, setStartStroke }, uber }:BrushFunctions<SerializedMarkerBrush>)=>{
+    let thatprevToLastPoint: Point = [0, 0];
+    let thatlastPoint: Point = [0, 0];
+    const thatbuffer:DrawableState = createDrawable({ size: [1, 1] });
+
+    setStartStroke((drawable:DrawableState, point:Point, color:string, alpha:number, width:number)=>{
+        thatlastPoint = point;
+        thatprevToLastPoint = point;
+        const { ctx, canvas } = drawable;
+        const { ctx: bufferCtx, canvas: buffer } = thatbuffer;
+        buffer.width = canvas.width;
+        buffer.height = canvas.height;
+        if (!ctx || !bufferCtx) return;
+        bufferCtx.lineCap = 'round';
+        bufferCtx.lineJoin = 'round';
+        bufferCtx.strokeStyle = color;
+        bufferCtx.lineWidth = width;
+        bufferCtx.imageSmoothingEnabled = false;
+        bufferCtx.filter = 'url(#no-anti-aliasing)';
+        ctx.globalCompositeOperation = 'source-over';
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.fillStyle = color;
+        ctx.globalAlpha = alpha;
+        canvas.style.filter = 'blur(1px)';
+        bufferCtx.globalCompositeOperation = 'copy';
+        bufferCtx.beginPath();
+        bufferCtx.moveTo(...point);
+        bufferCtx.lineTo(...point);
+        bufferCtx.stroke();
+        ctx.drawImage(buffer, 0, 0);
+    });
+
+    setDrawStroke((drawable:DrawableState, point:Point, color:string, alpha:number, width:number)=>{
+        const ctx = drawable.ctx;
+        const { ctx: bufferCtx, canvas } = thatbuffer;
+        if (!ctx || !bufferCtx) return;
+        bufferCtx.globalCompositeOperation = 'copy';
+        bufferCtx.beginPath();
+        bufferCtx.moveTo(...thatprevToLastPoint);
+        bufferCtx.lineTo(...thatlastPoint);
+        bufferCtx.stroke();
+        bufferCtx.globalCompositeOperation = 'source-out';
+        bufferCtx.beginPath();
+        bufferCtx.moveTo(...thatlastPoint);
+        bufferCtx.lineTo(...point);
+        bufferCtx.stroke();
+        ctx.drawImage(canvas, 0, 0);
+        thatprevToLastPoint = thatlastPoint;
+        thatlastPoint = point;
+    });
+
+    setEndStroke((drawable:DrawableState, point:Point, color:string, alpha:number, width:number)=>{
+        const { ctx, canvas } = drawable;
+        const { ctx: bufferCtx, canvas: canvas2 } = thatbuffer;
+        if (!ctx || !bufferCtx) return;
+        ctx?.restore();
+        bufferCtx.globalCompositeOperation = 'copy';
+        bufferCtx.filter = 'blur(1px)';
+        bufferCtx.drawImage(canvas, 0, 0);
+        canvas.width = 0;
+        canvas.height = 0;
+        canvas.width = canvas2.width;
+        canvas.height = canvas2.height;
+        ctx.globalCompositeOperation = 'copy';
+        ctx.drawImage(canvas2, 0, 0);
+        ctx.globalCompositeOperation = 'source-over';
+        ctx.filter = 'none';
+        canvas2.width = 0;
+        canvas2.height = 0;
+        canvas.style.filter = 'none';
+    });
+    return<></>;
+});

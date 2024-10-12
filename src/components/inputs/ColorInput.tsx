247d8fa9c55a7068ee95773ae4fb00a88cccc63d
JuanLP <juan.luis.palacios.p@gmail.com>
2024-10-11 23:14:45 -0500

    Add Palette dropdown on color select

--- src/components/inputs/ColorInput.tsx
+++ src/components/inputs/ColorInput.tsx
@@ -1,16 +1,35 @@
 import  '../../css/inputs/ColorInput.css';
 import { LeftMenuPortal } from '../portals/LeftMenu';
 import { useColorOptions } from '../../hooks/useColorOptions';
+import { uid } from '../../lib/uid';
+import { useMemo } from 'react';
+import { usePalette } from '../../hooks/usePalette';
 
 export const ColorInput = () => {
     const [props, onChange] = useColorOptions();
+    const [palette, { addColor }] = usePalette();
+    const id = useMemo(()=>uid(), []);
     const { color } = props;
     return <LeftMenuPortal>
-        <label className='ColorInput'>
+        <label className='ColorInput dropdown'>
+
+            <button>
                 color
-            <div className='color' style={{ background: color }}>
-                <input type="color" value={color} onChange={(e) => onChange({ color: e.target.value })} />
-            </div>
+                <div className='color' style={{ background: color }}>
+                </div>
+            </button>
+            <ul>
+                {palette.map((color, i) => <li key={id+'-'+i}>
+                    <div className='color' style={{ background: color }} onMouseDown={()=>onChange({ color })}></div>
+                </li>)}
+                <li>
+                    <div className='color'>
+                        +
+                        <input type="color" value={color} onMouseDown={(e)=>{ e.currentTarget.click(); }} onChange={(e) => onChange({ color: e.target.value })} onBlur={()=>addColor(color)} />
+                    </div>
+                </li>
+            </ul>
         </label>
     </LeftMenuPortal>;
 };
+

74fa2f56db47c2c1cae4c800054dcc59a47b1e12
JuanLP <juan.luis.palacios.p@gmail.com>
2024-10-22 23:45:15 -0500

    Add new smear icon

--- src/contexts/ToolOptionsContext.tsx
+++ src/contexts/ToolOptionsContext.tsx
@@ -10,6 +10,7 @@ import drawIcon from '../icons/brush-f-svgrepo-com.svg';
 import eraseIcon from '../icons/erase-svgrepo-com.svg';
 import fillIcon from '../icons/color-bucket-svgrepo-com.svg';
 import transformIcon from '../icons/nametag-svgrepo-com.svg';
+import smearIcon from '../icons/smear.svg';
 import { Smear } from '../tools/Smear';
 
 export const ToolOptionsContext = createContext<StatePair<ToolOptions>>([
@@ -24,7 +25,7 @@ export const ToolOptionsContextProvider = (props: { children: ReactNode; }) => {
             { key: uid(), Tool: Draw, name: 'draw', icon: drawIcon },
             { key: uid(), Tool: Erase, name: 'erase', icon: eraseIcon },
             { key: uid(), Tool: Fill, name: 'fill', icon: fillIcon },
-            { key: uid(), Tool: Smear, name: 'smear', icon: fillIcon },
+            { key: uid(), Tool: Smear, name: 'smear', icon: smearIcon },
             { key: uid(), Tool: Transform, name: 'transform', icon: transformIcon }
         ],
         selectedTool: 0

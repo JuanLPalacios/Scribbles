86fe6c9b698b6a0e6b8452707cbc9789f53ff366
JuanLP <juan.luis.palacios.p@gmail.com>
2024-11-04 05:48:53 -0500

    Add default custom brushes

--- src/hooks/useStoredBrushes.tsx
+++ src/hooks/useStoredBrushes.tsx
@@ -1,12 +1,9 @@
 //import round from '../brushes/stiff/round.json';
 //import oldRound from '../brushes/stiff/oldRound.json';
 //import diagonal from '../brushes/stiff/flat.json';
-import { BRUSH_TYPE_LIST } from '../abstracts/Brush';
+import default_brushes from '../brushes/default_brushes.json';
 import { createStorageHook } from '../generators/createStorageHook';
 import { SerializedBrush } from '../lib/Serialization';
 
-export const useStoredBrushes = createStorageHook<SerializedBrush[]>('brushes', 'local', [
-    ...BRUSH_TYPE_LIST.map(x => x[1])
-]
-);
+export const useStoredBrushes = createStorageHook<SerializedBrush[]>('brushes', 'local', default_brushes as SerializedBrush[]);
 

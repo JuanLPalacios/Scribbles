cbe72252a5573f0046c6963a6745ff496d532f80
Juan Palacios <juan.luis.palacios.p@gmail.com>
2025-12-26 23:52:52 -0500

    Add jsdom canvas support

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,66 +5,10 @@
 import '@testing-library/jest-dom';
 import Modal from 'react-modal';
 
-// Minimal canvas 2D context mock for tests running under jsdom
-// Provides the methods used by drawing/tools code so events can be exercised.
-if (typeof HTMLCanvasElement !== 'undefined') {
-    const mockCtx = () => {
-        const width = 1200;
-        const height = 800;
-        const imageData = {
-            data: new Uint8ClampedArray(width * height * 4),
-            width,
-            height,
-        } as ImageData;
-        return {
-            // state
-            canvas: { width, height } as HTMLCanvasElement,
-            // transforms & ops
-            save: () => void 0,
-            restore: () => void 0,
-            resetTransform: () => void 0,
-            setTransform: () => void 0,
-            translate: () => void 0,
-            rotate: () => void 0,
-            scale: () => void 0,
-            // drawing ops
-            beginPath: () => void 0,
-            closePath: () => void 0,
-            stroke: () => void 0,
-            fill: () => void 0,
-            fillRect: (_x: number, _y: number, _w: number, _h: number) => void 0,
-            strokeRect: (_x: number, _y: number, _w: number, _h: number) => void 0,
-            rect: (_x: number, _y: number, _w: number, _h: number) => void 0,
-            moveTo: (_x: number, _y: number) => void 0,
-            lineTo: (_x: number, _y: number) => void 0,
-            arc: (_x: number, _y: number, _r: number, _s: number, _e: number) => void 0,
-            quadraticCurveTo: (_cpx: number, _cpy: number, _x: number, _y: number) => void 0,
-            bezierCurveTo: (_cp1x: number, _cp1y: number, _cp2x: number, _cp2y: number, _x: number, _y: number) => void 0,
-            setLineDash: (_segments: number[]) => void 0,
-            drawImage: () => void 0,
-            clearRect: () => void 0,
-            putImageData: (_data: ImageData, _x: number, _y: number) => void 0,
-            getImageData: (_x: number, _y: number, _w: number, _h: number) => imageData,
-            createImageData: (w: number, h: number) => new ImageData(w, h),
-        } as unknown as CanvasRenderingContext2D;
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (HTMLCanvasElement.prototype as any).getContext = function (_type: string) {
-        return mockCtx();
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (HTMLCanvasElement.prototype as any).toDataURL = function () {
-        return 'data:image/png;base64,';
-    };
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (HTMLCanvasElement.prototype as any).toBlob = function (callback: (blob: Blob | null) => void, _type?: string, _quality?: number) {
-        const blob = new Blob([new Uint8Array([0])], { type: 'image/png' });
-        callback(blob);
-    };
-}
+// Canvas 2D context: provided by the canvas npm package via jsdom.
+// The canvas package (v3.2.0+) provides real canvas bindings for Node.js/jsdom tests.
+// If native binding issues occur on Windows, run `npm rebuild canvas` to recompile.
+// No custom mock needed; all canvas API methods work natively.
 
 // jsdom does not implement matchMedia; provide a simple mock used by QuickStart
 if (typeof window !== 'undefined' && !('matchMedia' in window)) {

4df1c46b36b5e3750a53030db82490a0e60c52b6
Juan Palacios <juan.luis.palacios.p@gmail.com>
2026-01-01 08:46:34 -0500

    Fix color input should close on clicking again on the value

--- src/components/inputs/ColorInput.tsx
+++ src/components/inputs/ColorInput.tsx
@@ -2,19 +2,33 @@ import  '../../css/inputs/ColorInput.css';
 import { LeftMenuPortal } from '../portals/LeftMenu';
 import { useColorOptions } from '../../hooks/useColorOptions';
 import { uid } from '../../lib/uid';
-import { useMemo } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { usePalette } from '../../hooks/usePalette';
 import { InputColor } from './InputColor';
 
 export const ColorInput = () => {
     const [props, onChange] = useColorOptions();
     const [palette, { addColor }] = usePalette();
+    const [alreadyOpen, setAlreadyOpen] = useState(false);
+    const buttonRef = useRef<HTMLButtonElement>(null);
     const id = useMemo(()=>uid(), []);
     const { color } = props;
     return <LeftMenuPortal>
         <label className='ColorInput dropdown'>
 
-            <button>
+            <button ref={buttonRef} onMouseDown={(e)=>{
+                // Toggle: if already focused within this dropdown
+                const dropdown = e.currentTarget.closest('.dropdown');
+                if (dropdown?.matches(':focus-within')) {
+                    setAlreadyOpen(true);                    return;
+                }
+            }} onClick={e=>{
+                // blur to close
+                if (alreadyOpen){
+                    e.currentTarget.blur();
+                    setAlreadyOpen(false);
+                }
+            }}>
                 color
                 <div className='color' style={{ background: color }}>
                 </div>

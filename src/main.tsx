d22f4dbf6d6e013e397e89774f6b1c9e2fabc7fb
Juan Palacios <juan.luis.palacios.p@gmail.com>
2026-01-06 23:58:27 -0500

    Fix missing context

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import ReactModal from 'react-modal';
 import App from './App';
 import { AppStateProvider } from './contexts/AppContext';
+import { DemoRecorderProvider } from './contexts/DemoRecorderContext';
+import { VideoRecorderProvider } from './contexts/VideoRecorderContext';
 import { content, overlay } from './css/Modal.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './css/index.css';
@@ -17,7 +19,11 @@ ReactModal.setAppElement(appElement);
 const root = ReactDOM.createRoot(appElement);
 root.render(
     <AppStateProvider>
-        <App />
+        <DemoRecorderProvider>
+            <VideoRecorderProvider>
+                <App />
+            </VideoRecorderProvider>
+        </DemoRecorderProvider>
     </AppStateProvider>);
 window.addEventListener('wheel', e=>{
     if (e.ctrlKey||e.deltaX !== 0)
